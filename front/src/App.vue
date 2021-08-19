aeb51c6febccb91b5b464197a58038f035a99aa5
Alix <a.boufflers@it-students.fr>
2021-08-19 11:46:51 +0200

    :tada: front init

---
+++ front/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <Header>
+      <router-link to="/">Home</router-link> |
+    </Header>
+    <router-view/>
+  </div>
+</template>
+<script>
+import Header from './components/Header';
+export default {
+  components:{
+    Header
+  }
+}
+</script>
+<style>
+#app {
+  font-family: Roboto, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #171322;
+}
+</style>
---
+++ front/src/components/Event.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+   <b-card
+   :title="title"
+   :img-src="cover_url"
+   img-top
+   style="width: 20rem;"
+    class="m-2"
+   >
+  <b-card-text class="container">
+    {{title}}
+    {{category}}
+    {{description}}
+    {{price}}
+  </b-card-text>
+  <b-button :href="url">Plus d'info</b-button>
+   </b-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Event',
+  props: {
+    link: String,
+    address_city: String,
+    address_name: String,
+    address_street: String,
+    address_zipcode: String,
+    category: String,
+    facebook: String,
+    twitter: String,
+    cover_url: String,
+    date_escription: String,
+    date_start: String,
+    date_end: String,
+    description: String,
+    lead_text: String,
+    price: String,
+    title: String,
+    url: String,
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
